de57b9df4a63a9623a5ebdca3e3ed8c0f7a5e3e1
Faigy Shternhel <f801010310@gmail.com>
2023-06-08 12:11:34 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import React from 'react';
+import AppHw from './appHw';
 import './App.css';
 
 function App() {
--- src/appHw.js
+++ src/appHw.js
@@ -1,11 +1,13 @@
 import React,{Component} from 'react'
 import Message from './massege' 
+import Exchange from './exchange'
 export default class AppHw extends Component{
     render(){
         return(
             <div className='container'>
                 <Message msg = "First 1" bg = "red"/>
-                <Message msg = "secend 2" bg = "rgd(0,255,255)"/>
+                <Message msg = "secend 2" bg = "rgb(0,255,255)"/>
+                <Exchange />
             </div>
         )
     }
