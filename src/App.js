b457896f33d6ad7636088a049f3f70b7cdd86eb7
Faigy Shternhel <f801010310@gmail.com>
2023-06-12 09:55:41 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+// import CarsList from './carsList'
+import ParentComp from './parentComp'
+import AppHw from './comp_HW/appHw'
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <hr />
+        <AppHw />
+        <hr />
+      </React.Fragment>
+    )
+  }
 }
-
-export default App;
