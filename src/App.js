fa71fd980a72f443f6c261c0f7d4a35bc609f86e
Faigy Shternhel <f801010310@gmail.com>
2023-06-12 15:01:52 +0300

    fuin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import React, { Component } from 'react'
 import AppTv from './hooks_comps/appTv'
+import AppWeather from './hooks_comps/appWeather'
 
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
         <hr />
-        <AppTv />
+        <AppWeather />
         <hr />
       </React.Fragment>
     )
