ff44af7ef2bd35ca2292dfe88eeb1e9089d53928
Faigy Shternhel <f801010310@gmail.com>
2023-06-20 20:58:06 +0300

    finish1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
-import React, { Component } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  return (
 
-export default class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <hr />
-        <h1>hollo</h1>
-        <hr />
-      </React.Fragment>
-    )
-  }
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />}/>
+          <Route path="/product/:id" element={<Product />}/>
+
+            <Route path="/cart" element={<Success />}/>
+
+              <Route path="/success" element={<Success />}/>
+              <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+              <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+              </Routes>
+            </Router>
+
+            );
+};
+
+            export default App;
